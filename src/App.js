36939c31b842c9adf08207e54155ed67e075d870
Abdallah Smadi <abd.smadi41@gmail.com>
2022-08-13 16:54:33 +0300

    Create Routes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,38 @@ import Header from "./componants/header/header";
 import CardsComponant from "./componants/home/cards/cards";
 import CarouselComponant from "./componants/home/carousel/carousel";
 import SecondaryNav from "./componants/home/secondaryNavbar/secondaryNav";
+import About from "./componants/About";
+import Contact from "./componants/Contact";
+import Footer from "./componants/footer/footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import Admin from "./componants/Admin";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <SecondaryNav />
-      <CarouselComponant />
-      <CardsComponant />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <SecondaryNav />
+      </div>
+
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <CarouselComponant /> <CardsComponant />
+            </>
+          }
+        ></Route>
+        <Route exact path="/about" element={<About />}></Route>
+        <Route exact path="/contact-us" element={<Contact />}></Route>
+        <Route exact path="/admin" element={<Admin />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- src/componants/header/header.js
+++ src/componants/header/header.js
@@ -17,9 +17,8 @@ export default class Header extends Component {
             >
               <Nav>
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/">About</Nav.Link>
-                <Nav.Link href="/">Contact Us</Nav.Link>
-                <Nav.Link href="/">Admin</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="/contact-us">Contact Us</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
