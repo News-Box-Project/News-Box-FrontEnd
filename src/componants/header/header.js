16ae447322fc636f8d329a5b275951d60ea558c1
SuhaibAln <201710390@stu.meu.edu.jo>
2022-08-13 18:25:03 +0300

    added about us

--- src/componants/header/header.js
+++ src/componants/header/header.js
@@ -1,4 +1,4 @@
-import react, { Component } from "react";
+import  { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
