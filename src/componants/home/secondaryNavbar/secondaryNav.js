a6a33cdeb24d1121fa662c4192f9e1ad4d506afe
Wlla Talafha <wlla.issa396@gmail.com>
2022-08-14 13:50:20 +0300

    refactorig the home page

--- src/componants/home/secondaryNavbar/secondaryNav.js
+++ src/componants/home/secondaryNavbar/secondaryNav.js
@@ -1,77 +1,10 @@
-import axios from "axios";
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import CardsComponant from "../cards/cards";
-import CarouselComponant from "../carousel/carousel";
 import "./secondaryNav.css";
 
 export default class SecondaryNav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      news: [],
-    };
-  }
-
-  async componentDidMount() {
-    const result = await axios.get(
-      "https://news-box-project.herokuapp.com/news"
-    );
-    
-    this.setState({
-      news: result.data,
-    });
-  }
-
-  handleChange = async (category) => {
-    const result = await axios.get(
-      `https://news-box-project.herokuapp.com/category?categories=${category}`
-    );
-   
-    this.setState({
-      news: result.data,
-    });
-  };
-
-  handleSearch = async(e) => {
-    e.preventDefault();
-    const keyword=e.target.inputValue.value;
-    const url=`https://news-box-project.herokuapp.com/apinews?keywords=${keyword}`;
-    const newsData= await axios.get(url);
-    this.setState({
-      news:newsData.data
-    })
-  };
-
-  handleNewsBox = () => {
-    this.componentDidMount();
-  };
-
-  handleSport = () => {
-    this.handleChange("sports");
-  };
-
-  handleHealth = () => {
-    this.handleChange("health");
-  };
-
-  handleBusiness = () => {
-    this.handleChange("business");
-  };
-
-  handleEntertainment = () => {
-    this.handleChange("entertainment");
-  };
-
-  handleScience = () => {
-    this.handleChange("science");
-  };
-
-  handleTechnology = () => {
-    this.handleChange("technology");
-  };
 
   render() {
     return (
@@ -81,43 +14,43 @@ export default class SecondaryNav extends React.Component {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav>
                 <Nav.Link
-                  onClick={this.handleNewsBox}
+                  onClick={this.props.handleNewsBox}
                   className="secondary-nav-link"
                 >
                   News Box
                 </Nav.Link>
                 <Nav.Link
-                  onClick={this.handleSport}
+                  onClick={this.props.handleSport}
                   className="secondary-nav-link"
                 >
                   Sport
                 </Nav.Link>
                 <Nav.Link
-                  onClick={this.handleTechnology}
+                  onClick={this.props.handleTechnology}
                   className="secondary-nav-link"
                 >
                   Technology
                 </Nav.Link>
                 <Nav.Link
-                  onClick={this.handleEntertainment}
+                  onClick={this.props.handleEntertainment}
                   className="secondary-nav-link"
                 >
                   Entertainment
                 </Nav.Link>
                 <Nav.Link
-                  onClick={this.handleBusiness}
+                  onClick={this.props.handleBusiness}
                   className="secondary-nav-link"
                 >
                   Business
                 </Nav.Link>
                 <Nav.Link
-                  onClick={this.handleHealth}
+                  onClick={this.props.handleHealth}
                   className="secondary-nav-link"
                 >
                   Health
                 </Nav.Link>
                 <Nav.Link
-                  onClick={this.handleScience}
+                  onClick={this.props.handleScience}
                   className="secondary-nav-link"
                 >
                   Science
@@ -125,7 +58,7 @@ export default class SecondaryNav extends React.Component {
               </Nav>
             </Navbar.Collapse>
             <div className="search d-flex gap-2">
-              <form className="d-flex gap-3" onSubmit={this.handleSearch}>
+              <form className="d-flex gap-3" onSubmit={this.props.handleSearch}>
                 <input
                   type="text"
                   placeholder="Search"
@@ -139,8 +72,6 @@ export default class SecondaryNav extends React.Component {
             </div>
           </Container>
         </Navbar>
-        <CarouselComponant news={this.state.news} />
-        <CardsComponant news={this.state.news} />
       </div>
     );
   }
